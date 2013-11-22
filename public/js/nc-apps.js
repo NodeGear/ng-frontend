57adf8a34e0d0873f2a3dbfa538b869a399b9562
Matej Kramny <matejkramny@gmail.com>
2013-11-22 09:37:01 +0000

    Basic Graphs for usage

--- public/js/nc-apps.js
+++ public/js/nc-apps.js
@@ -8,6 +8,7 @@ angular.module('nodecloud')
 	$scope.app = data.app || {}
 	
 	$scope.log = null
+	$scope.usage = [];
 	
 	$scope.selectLog = function (log) {
 		$scope.log = log
@@ -15,6 +16,37 @@ angular.module('nodecloud')
 		$http.get('/app/'+$scope.app._id+'/log/'+log._id).success(function(data, status) {
 			$scope.log = data.log;
 		})
+	}
+	
+	$scope.getUsage = function () {
+		$http.get('/app/'+$scope.app._id+'/usage').success(function(data, status) {
+			for (var i = 0; i < data.usage.length; i++) {
+				data.usage[i].time = new Date(data.usage[i].time)
+				data.usage[i].memory = data.usage[i].memory / 1024 / 1024;
+			}
+			
+			$scope.usage = data.usage;
+			
+			$scope.plotGraphs()
+		})
+	}
+	
+	$scope.plotGraphs = function () {
+		var mem = [], cpu = [];
+		
+		for (var i = 0; i < $scope.usage.length; i++) {
+			var u = $scope.usage[i];
+			mem.push([u.time.getHours()+""+u.time.getMinutes(), u.memory])
+			cpu.push([u.time.getHours()+""+u.time.getMinutes(), u.cpu])
+		}
+		
+		$.plot("#usageGraph", [{
+			data: cpu,
+			lines: { show: true }
+		}, {
+			data: mem,
+			lines: { show: true, fill: true }
+		}])
 	}
 	
 	if ($scope.app.logs && $scope.app.logs.length > 0) {
