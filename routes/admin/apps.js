905311df4d91ea09bf195546796298b20afc1dac
Matej Kramny <matejkramny@gmail.com>
2014-02-05 13:39:41 +0000

    Show app and user

--- routes/admin/apps.js
+++ routes/admin/apps.js
@@ -2,6 +2,7 @@ var models = require('../../models')
 
 exports.router = function (app) {
 	app.get('/admin/apps', getApps)
+		.get('/admin/app/:id', getApp, showApp)
 }
 
 function getApps (req, res) {
@@ -10,4 +11,17 @@ function getApps (req, res) {
 		
 		res.render('admin/apps')
 	})
+}
+
+function getApp (req, res, next) {
+	var id = req.params.id;
+	
+	models.Drone.findById(id).populate('user').exec(function(err, app) {
+		res.locals.app = app;
+		next();
+	})
+}
+
+function showApp (req, res) {
+	res.render('admin/app')
 }
--- routes/admin/users.js
+++ routes/admin/users.js
@@ -2,6 +2,7 @@ var models = require('../../models')
 
 exports.router = function (app) {
 	app.get('/admin/users', getUsers)
+		.get('/admin/user/:id', getUser, showUser)
 }
 
 function getUsers (req, res) {
@@ -10,4 +11,16 @@ function getUsers (req, res) {
 		
 		res.render('admin/users')
 	})
+}
+
+function getUser (req, res, next) {
+	console.log("WTF?")
+	models.User.findById(req.params.id, function(err, user) {
+		res.locals.aUser = user;
+		next();
+	});
+}
+
+function showUser (req, res) {
+	res.render('admin/user')
 }
