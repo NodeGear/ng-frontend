ae78db81d13edb5e930b68058f549ad72d37b7db
Matej Kramny <matejkramny@gmail.com>
2014-04-11 00:36:48 +0100

    Patch test/prepare for new db models

--- test/prepare.js
+++ test/prepare.js
@@ -12,36 +12,61 @@ if (!process.env.NG_TEST) {
 should(process.env.NG_TEST).be.ok;
 
 it('clean the database', function(done) {
+	// better solution required
 	require('async').parallel([
-		function(cb) {
-			models.Analytic.remove(cb)
+		function (cb) {
+			models.Analytic.remove(cb);
 		},
-		function(cb) {
-			models.App.remove(cb)
+		function (cb) {
+			models.App.remove(cb);
 		},
-		function(cb) {
-			models.Event.remove(cb)
+		function (cb) {
+			models.AppDomain.remove(cb);
 		},
-		function(cb) {
-			models.PaymentMethod.remove(cb)
+		function (cb) {
+			models.AppEnvironment.remove(cb);
 		},
-		function(cb) {
-			models.PublicKey.remove(cb)
+		function (cb) {
+			models.AppEvent.remove(cb);
 		},
-		function(cb) {
-			models.TFA.remove(cb)
+		function (cb) {
+			models.AppLog.remove(cb);
 		},
-		function(cb) {
-			models.Ticket.remove(cb)
+		function (cb) {
+			models.AppProcess.remove(cb);
 		},
-		function(cb) {
-			models.Transaction.remove(cb)
+		function (cb) {
+			models.EmailVerification.remove(cb);
 		},
-		function(cb) {
-			models.Usage.remove(cb)
+		function (cb) {
+			models.ForgotNotification.remove(cb);
 		},
-		function(cb) {
-			models.User.remove(cb)
+		function (cb) {
+			models.NetworkPerformanceRaw.remove(cb);
+		},
+		function (cb) {
+			models.PaymentMethod.remove(cb);
+		},
+		function (cb) {
+			models.PublicKey.remove(cb);
+		},
+		function (cb) {
+			models.Server.remove(cb);
+		},
+		function (cb) {
+			models.TFA.remove(cb);
+		},
+		function (cb) {
+			models.Ticket.remove(cb);
+		},
+		function (cb) {
+			models.Transaction.remove(cb);
+		},
+		function (cb) {
+			models.Usage.remove(cb);
+		},
+		function (cb) {
+			models.User.remove(cb);
 		}
 	], done)
 })
