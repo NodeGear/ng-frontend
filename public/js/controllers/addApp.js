35a00f6585ad320a53caf4de8ad8a12b97331f95
Matej Kramny <matejkramny@gmail.com>
2014-08-09 20:24:31 +0100

    Fix Email sending issue, Complete Add App UI

--- public/js/controllers/addApp.js
+++ public/js/controllers/addApp.js
@@ -1,12 +1,9 @@
 define([
-	'angular',
 	'app',
-	'moment',
-	'socketio',
 	'async',
 	'../services/servers',
 	'../services/user'
-], function(angular, app, moment, io, async) {
+], function(app, async) {
 	app.registerController('CreateAppStepCtrl', function ($scope, servers) {
 		$scope.step = 0;
 
@@ -106,13 +103,13 @@ define([
 
 			var data = {
 				domain: {
-					domain: application.app_url+'-'+user.user.username+'.ngapp.io',
+					domain: application.nameUrl+'-'+user.user.username+'.ngapp.io',
 					ssl: true,
 					ssl_only: false
 				}
 			};
 
-			var url = '/app/'+application.app_url+'/domain';
+			var url = '/app/'+application.nameUrl+'/domain';
 
 			$http.post(url, data).success(function(data, status) {
 				if (data.status == 200) {
@@ -132,6 +129,8 @@ define([
 					name: $scope.status,
 					class: "fa-times"
 				});
+
+				callback("");
 			})
 		}
 
@@ -142,224 +141,257 @@ define([
 				class: "fa-check"
 			});
 
-			callback(null, application);
+			callback(null, application, domain);
+		}
+
+		$scope.createDatabaseStep = function (application, domain, callback) {
+			$scope.status = "Creating Database..";
+			$scope.setProperty('database', {
+				name: $scope.status,
+				class: "fa-circle-o-notch fa-spin"
+			});
+
+			$http.post('/database', {
+				database: {
+					name: application.nameUrl,
+					database_type: 'mysql'
+				}
+			})
+			.success(function (data) {
+				callback(null, application, domain, data.database);
+			})
+			.error(function (data) {
+				$scope.status = "Database Failed to Create due to an Unknown Issue.";
+				$scope.setProperty('database', {
+					name: $scope.status,
+					class: "fa-times"
+				});
+
+				callback("");
+			})
+		}
+
+		$scope.finishDatabaseStep = function (application, domain, database, callback) {
+			$scope.status = "MySQL Database Created.";
+			$scope.setProperty('database', {
+				name: $scope.status,
+				class: "fa-check"
+			});
+		}
+
+		$scope.createEnvironmentStep = function (application, domain, database, callback) {
+			$scope.status = "Configuring Environment..";
+			$scope.setProperty('environment', {
+				name: $scope.status,
+				class: "fa-circle-o-notch fa-spin"
+			});
+
+			var envs = [
+				['DATABASE', 'true'],
+				['DATABASE_HOST', database.db_host],
+				['DATABASE_USER', database.db_user],
+				['DATABASE_PASSWORD', database.db_pass],
+				['DATABASE_NAME', database.db_name],
+				['DOMAIN', domain]
+			];
+
+			async.each(envs, function (env, done) {
+				var data = {
+					env: {
+						name: env[0],
+						value: env[1]
+					}
+				};
+
+				$http.post('/app/'+$scope.app_url+'/environment', data)
+				.success(function () {
+					done();
+				})
+				.error(function (data, status) {
+					done(status);
+				});
+			}, function (err) {
+				if (err) {
+					$scope.status = "Environment Failed to Configure due to an Unknown Issue. (Error "+err+")";
+					$scope.setProperty('environment', {
+						name: $scope.status,
+						class: "fa-times"
+					});
+
+					return callback(err);
+				}
+
+				callback(null, application, domain, database);
+			});
+		}
+
+		$scope.finishEnvironmentStep = function (application, domain, database, callback) {
+			$scope.status = "Environment Configured.";
+			$scope.setProperty('environment', {
+				name: $scope.status,
+				class: "fa-check"
+			});
+
+			callback(null, application, domain);
+		}
+
+		$scope.createProcessStep = function (application, domain, callback) {
+			$scope.status = "Creating a Process..";
+			$scope.setProperty('process-create', {
+				name: $scope.status,
+				class: "fa-circle-o-notch fa-spin"
+			});
+
+			var data = {
+				process: {
+					name: $scope.app.name,
+					server: $scope.server
+				}
+			};
+			var url = '/app/'+ application.nameUrl +'/process';
+
+			$http.post(url, data).success(function(data, status) {
+				if (data.status != 200) {
+					$scope.status = data.message;
+					$scope.setProperty('process-create', {
+						name: data.message,
+						class: "fa-times"
+					});
+
+					return callback(data.message);
+				}
+
+				callback(null, application, domain, data.process);
+			}).error(function() {
+				$scope.status = "Process Failed to Create due to an Unknown Issue";
+				$scope.setProperty('process-create', {
+					name: $scope.status,
+					class: "fa-times"
+				});
+
+				return callback(err);
+			});
+		}
+
+		$scope.finishProcessStep = function (application, domain, process, callback) {
+			$scope.status = "Process Created.";
+
+			$scope.setProperty('process-create', {
+				name: $scope.status,
+				class: "fa-check"
+			});
+
+			callback(null, application, domain, process);
+		}
+
+		$scope.bootProcessStep = function (application, domain, process, callback) {
+			$scope.status = "Booting a Process..";
+			$scope.setProperty('process-boot', {
+				name: $scope.status,
+				class: "fa-circle-o-notch fa-spin"
+			});
+
+			$http.post('/app/'+ application.nameUrl +'/process/'+ process +'/start')
+			.success(function(data) {
+				if (data.status == 200) {
+					callback(null, application, domain, process);
+				} else {
+					$scope.status = "Process Failed to start.";
+					$scope.setProperty('process-boot', {
+						name: $scope.status,
+						class: "fa-times"
+					});
+
+					callback($scope.status);
+				}
+			}).error(function() {
+				$scope.status = "Failed to Create Process";
+				$scope.setProperty('process-boot', {
+					name: $scope.status,
+					class: "fa-times"
+				});
+
+				callback('')
+			});
+		}
+
+		$scope.finishBootStep = function (application, domain, process, callback) {
+			$scope.status = "Process Start Scheduled.";
+			$scope.setProperty('process-boot', {
+				name: $scope.status,
+				class: "fa-check"
+			});
+
+			callback(null, application, domain, process);
+		}
+
+		$scope.doneStep = function (application, domain, process, callback) {
+			$scope.status = "Application Created.";
+
+			$scope.setProperty('finished', {
+				name: $scope.status,
+				class: "fa-check"
+			});
+
+			callback(null);
+		}
+
+		$scope.delayStep = function () {
+			// Convert arguments to a real array
+			var args = Array.prototype.slice.call(arguments);
+
+			// Pop the callback argument (its always last)
+			var callback = args.pop();
+
+			// The error argument
+			args.splice(0, 0, null);
+
+			setTimeout(function() {
+				callback.apply(this, args);
+			}, 1000);
 		}
 
 		var steps = [
-			$scope.createApplicationStep,
+			$scope.createApplicationStep, $scope.delayStep,
 			$scope.finishApplicationStep,
-			$scope.createDomainStep,
+			$scope.createDomainStep, $scope.delayStep,
 			$scope.finishDomainStep
 		];
 
 		if ($scope.app.template == 'ghost') {
 			// Create a database..
-			steps.push($scope.createDatabaseStep, $scope.finishDatabaseStep);
+			steps.push($scope.createDatabaseStep, $scope.delayStep, $scope.finishDatabaseStep);
+
+			// Environment variables
+			steps.push($scope.createEnvironmentStep, $scope.delayStep, $scope.finishEnvironmentStep);
 		}
 
-		// Environment variables
-		steps.push($scope.createEnvironmentStep, $scope.finishEnvironmentStep);
 		// Create process
-		steps.push($scope.createProcessStep, $scope.finishProcessStep);
+		steps.push($scope.createProcessStep, $scope.delayStep, $scope.finishProcessStep);
 
 		// Boot process
-		steps.push($scope.bootProcessStep, $scope.finishBootStep);
+		steps.push($scope.bootProcessStep, $scope.delayStep, $scope.finishBootStep);
 
 		steps.push($scope.doneStep);
 
-		async.waterfall(steps, function () {
-			$scope.setProperty('finished', {
-				name: "All Done :)",
-				class: "fa-check"
-			});
-		})
-	});
+		async.waterfall(steps, function (err) {
+			if (err) {
+				$scope.setProperty('finished', {
+					name: "App Creation Failed. "+err,
+					class: "fa-times"
+				});
+			} else {
+				$scope.setProperty('finished', {
+					name: "All Done :)",
+					class: "fa-check"
+				});
+			}
 
-	app.registerController('AddAppController', function ($scope, $http, $rootScope, servers, user, $sce) {
-		$scope.creating = false;
-		$scope.status = "";
-		$scope.running = true;
-		$scope.processid = "";
-		$scope.logs = [];
-		$scope.servers = [];
+			$scope.isDone = true;
 
-		servers.getServers(function(servers) {
-			$scope.servers = servers;
-			try {
-				if (!$scope.server) $scope.server = $scope.servers[0]._id;
-			} catch (e) {}
+			if (!$scope.$$phase) {
+				$scope.$digest();
+			}
 		});
-
-		var process_log = io('/process_log'),
-			app_running = io('/app_running');
-
-		$scope.$on('$destroy', function() {
-			process_log.removeListener('process_log', $scope.processLog);
-			app_running.removeListener('app_running', $scope.app_running);
-
-			process_log.emit('unsubscribe_log', {
-				id: $scope.app_id,
-				pid: $scope.processid
-			});
-		})
-		
-		$scope.create = function () {
-		}
-
-		$scope.createDatabase = function (type, cb) {
-			$scope.status = "Creating Database..";
-
-			$http.post('/database', {
-				database: {
-					name: $scope.app_url,
-					database_type: type
-				}
-			})
-			.success(function (data) {
-				$scope.status = "Database Created..";
-				$scope.database = data.database;
-
-				var done = 0;
-				var domain = $scope.app_url+'.'+user.user.username+'.ngapp.io';
-				var envs = [
-					['DATABASE', 'true'],
-					['DATABASE_HOST', $scope.database.db_host],
-					['DATABASE_USER', $scope.database.db_user],
-					['DATABASE_PASSWORD', $scope.database.db_pass],
-					['DATABASE_NAME', $scope.database.db_name],
-					['DOMAIN', domain]
-				];
-				var fn = function (iter) {
-					var data = {
-						env: {
-							name: envs[iter][0],
-							value: envs[iter][1]
-						}
-					};
-					$http.post('/app/'+$scope.app_url+'/environment', data)
-					.success(function (data) {
-						done++;
-						if (done >= envs.length) {
-							var fn = cb;
-
-							cb = function(){};
-							fn();
-						}
-					})
-				}
-
-				for (var i = 0; i < envs.length; i++) {
-					fn(i);
-				}
-			})
-			.error(function (data) {
-				$scope.status = "Could not create database.";
-			})
-		}
-
-		$scope.createDomain = function() {
-		}
-
-		$scope.addProcess = function() {
-			$scope.status = "Creating Process..";
-
-			var data = {
-				_csrf: csrf.csrf,
-				process: {
-					name: $scope.name,
-					server: $scope.server
-				}
-			};
-			var url = '/app/'+$scope.app_url+'/process';
-
-			$http.post(url, data).success(function(data, status) {
-				if (data.status == 200) {
-					$scope.status = "Process Added";
-					$scope.processid = data.process;
-
-					if ($scope.template == 'ghost') {
-						$scope.createDatabase('mysql', function() {
-							$scope.startProcess(data.process);
-						})
-					} else {
-						$scope.startProcess(data.process);
-					}
-				} else {
-					$scope.status = data.message;
-				}
-
-				if (!$scope.$$phase) {
-					$scope.$digest();
-				}
-			}).error(function() {
-				$scope.status = "The Request has failed.";
-
-				if (!$scope.$$phase) {
-					$scope.$digest();
-				}
-			});
-		}
-
-		$scope.startProcess = function(process) {
-			$scope.status = "Starting Process..";
-
-			process_log.on('process_log', $scope.processLog);
-			app_running.on('app_running', $scope.app_running);
-
-			process_log.emit('subscribe_log', {
-				id: $scope.app_id,
-				pid: process
-			});
-
-			$http.post('/app/'+$scope.app_id+'/process/'+process+'/start', {
-				_csrf: csrf.csrf
-			}).success(function(data) {
-				if (data.status == 200) {
-					$scope.status = "Process Starting..";
-					$scope.creating = true;
-				} else {
-					$scope.status = "Process Failed to start.";
-					$scope.creating = true;
-				}
-
-				if (!$scope.$$phase) {
-					$scope.$digest();
-				}
-			}).error(function() {
-				$scope.status = "The Request has Failed";
-				if (!$scope.$$phase) {
-					$scope.$digest();
-				}
-			})
-		}
-
-		$scope.processLog = function(data) {
-			if (data.pid == $scope.processid) {
-				$scope.logs.splice(0, 0, data.log);
-			}
-
-			if ($scope.logs.length > 100) {
-				$scope.logs = $scope.logs.slice(0, 100);
-			}
-
-			if (!$scope.$$phase) {
-				$scope.$digest();
-			}
-		}
-
-		$scope.app_running = function(data) {
-			if (data._id != $scope.processid) {
-				return;
-			}
-
-			$scope.is_running = data.running;
-			if ($scope.is_running == true) {
-				$scope.status = "Process Started!";
-			}
-
-			if (!$scope.$$phase) {
-				$scope.$digest();
-			}
-		}
-	})
+	});
 });
